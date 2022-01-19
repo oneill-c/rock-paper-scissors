8f042236bc8977df4b1aae3f5e44631a81fcc29f
mcpolandc <mcpolandc@gmail.com>
2022-01-18 19:25:08 -0600

    add layout back in

---
+++ src/components/layout.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode, ReactElement, useState } from 'react';
+import classNames from 'classnames';
+
+import Button from './button';
+import Modal from './modal';
+import TitleBox from './title-box/title-box';
+
+type Props = {
+  score: number;
+  children?: ReactNode;
+};
+
+const Layout = ({ score, children }: Props): ReactElement => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const toggleModal = () => {
+    return setIsModalOpen(!isModalOpen);
+  };
+
+  const classes = classNames(
+    'fixed',
+    'font-barlow',
+    'w-full',
+    'h-full',
+    'bg-gradient-radial',
+    'from-blue-1',
+    'to-blue-2'
+  );
+  const containerClasses = classNames('flex', 'flex-col', 'space-y-40', 'mx-8', 'mt-8', 'md:mx-60');
+
+  return (
+    <div className={classes}>
+      <div className={containerClasses}>
+        <TitleBox score={score} />
+        {children}
+        <div className="self-center md:self-end">
+          <Button onClick={toggleModal}>Rules</Button>
+        </div>
+      </div>
+      <Modal title="Rules" open={isModalOpen} onClose={toggleModal} center>
+        <img className="mx-auto" src="/assets/images/image-rules.svg" height={300} width={300} />
+      </Modal>
+    </div>
+  );
+};
+
+export default Layout;
